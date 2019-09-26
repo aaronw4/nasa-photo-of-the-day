b8dc1b96247c5484f41ba7b4714b3e07cec33f34
Aaron Williams <aaronrw4@yahoo.com>
2019-09-25 21:40:59 -0700

    Fix CSS for menu

--- src/components/DateButton.js
+++ src/components/DateButton.js
@@ -2,20 +2,18 @@ import React from 'react';
 import moment from "moment";
 
 export function Date(props) {
-    let oneDay = moment(props.date).subtract(1, 'days').calendar('YYYY-MM-DD');
-    let twoDays = moment(props.date).subtract(2, 'days').calendar('YYYY-MM-DD');
-    let threeDays = moment(props.date).subtract(3, 'days').calendar('YYYY-MM-DD');
-    let fourDays = moment(props.date).subtract(4, 'days').calendar('YYYY-MM-DD');
-    let fiveDays = moment(props.date).subtract(5, 'days').calendar('YYYY-MM-DD');
-    let sixDays = moment(props.date).subtract(6, 'days').calendar('YYYY-MM-DD');
-    let sevenDays = moment(props.date).subtract(7, 'days').calendar('YYYY-MM-DD');
-
-    console.log(oneDay)
+    let oneDay = moment(props.date).subtract(1, 'days').format('YYYY-MM-DD');
+    let twoDays = moment(props.date).subtract(2, 'days').format('YYYY-MM-DD');
+    let threeDays = moment(props.date).subtract(3, 'days').format('YYYY-MM-DD');
+    let fourDays = moment(props.date).subtract(4, 'days').format('YYYY-MM-DD');
+    let fiveDays = moment(props.date).subtract(5, 'days').format('YYYY-MM-DD');
+    let sixDays = moment(props.date).subtract(6, 'days').format('YYYY-MM-DD');
+    let sevenDays = moment(props.date).subtract(7, 'days').format('YYYY-MM-DD');
 
 return(
-<div class="dropdown">
-    <button class="dropbtn text">{props.date}</button>
-        <div class="dropdown-content">
+<div className="dropdown">
+    <button className="dropbtn text">{props.date}</button>
+        <div className="dropdown-content">
             <button>{oneDay}</button>
             <button>{twoDays}</button>        
             <button>{threeDays}</button>

291661db05af8811080df97e5753e2a4e6d24127
Aaron Williams <aaronrw4@yahoo.com>
2019-09-26 21:54:40 -0700

    set conditions on menu buttons

--- src/components/DateButton.js
+++ src/components/DateButton.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import moment from "moment";
+moment().format();
 
 export function Date(props) {
     let today = moment().format('YYYY-MM-DD');
-
+    let difference = moment(today).diff(moment(props.date), 'days');
+    
     let threeDaysBack = moment(props.date).add(3, 'days').format('YYYY-MM-DD');
     let twoDaysBack = moment(props.date).add(2, 'days').format('YYYY-MM-DD');
     let oneDayBack = moment(props.date).add(1, 'days').format('YYYY-MM-DD');
@@ -19,17 +21,17 @@ return(
 <div className="dropdown">
     <button className="dropbtn text">Change Date</button>
         <div className="dropdown-content">
-            <button onClick={props.changeDate} value={threeDaysBack}>{oneDayBack}</button>
-            <button onClick={props.changeDate} value={twoDaysBack}>{oneDayBack}</button>
-            <button onClick={props.changeDate} value={oneDayBack}>{oneDayBack}</button>
+            {difference > 2 ? <button onClick={props.changeDate} value={threeDaysBack} id='three'>{threeDaysBack}</button> : null}
+            {difference > 1 ? <button onClick={props.changeDate} value={twoDaysBack} id='two'>{twoDaysBack}</button> : null}
+            {difference > 0 ? <button onClick={props.changeDate} value={oneDayBack} id='one'>{oneDayBack}</button> : null}
             <button id='active'>{props.date}</button>
             <button onClick={props.changeDate} value={oneDay}>{oneDay}</button>
             <button onClick={props.changeDate} value={twoDays}>{twoDays}</button>        
             <button onClick={props.changeDate} value={threeDays}>{threeDays}</button>
             <button onClick={props.changeDate} value={fourDays}>{fourDays}</button>
-            <button onClick={props.changeDate} value={fiveDays}>{fiveDays}</button>
-            <button onClick={props.changeDate} value={sixDays}>{sixDays}</button>
-            <button onClick={props.changeDate} value={sevenDays}>{sevenDays}</button>     
+            {difference > 2 ? null : <button onClick={props.changeDate} value={fiveDays}>{fiveDays}</button>}
+            {difference > 1 ? null : <button onClick={props.changeDate} value={sixDays}>{sixDays}</button>}
+            {difference > 0 ? null : <button onClick={props.changeDate} value={sevenDays}>{sevenDays}</button>} 
         </div>
 </div>
 )}
